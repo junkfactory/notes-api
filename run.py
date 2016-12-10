3ea5d02fac8ee03229e7ef32df245407e65f6031
amontecillo <amontecillo@marqeta.com>
2016-12-10 12:42:58 -0800

    Use manager to run app

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 from eve import Eve
 from app.notes import pre_insert_notes
+from flask_script import Manager
 
 app = Eve()
 app.on_insert_notes += pre_insert_notes
 
+manager = Manager(app)
+
 if __name__ == '__main__':
-    app.run()
+    manager.run()
